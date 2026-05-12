d15c92ec14ef211f727e8dd5212daba071b41e6e
Andrew <andrew.alan.sm@gmail.com>
2026-05-12 16:37:04 +0100

    added scrollbar styling to hourly forecast group.

--- src/components/HourlyForecastGroup.tsx
+++ src/components/HourlyForecastGroup.tsx
@@ -45,11 +45,14 @@ export default function HourlyForecastGroup({
 }
 
 const Wrapper = styled.div`
+  --inline-padding: 16px;
+
   background-color: var(--clr-neutral-800);
   border-radius: 20px;
-  padding: 20px 16px;
-  overflow-y: scroll;
+  padding: 20px var(--inline-padding);
   height: ${693 / 16}rem;
+  display: flex;
+  flex-direction: column;
 `;
 
 const SectionHeader = styled.div`
@@ -64,7 +67,35 @@ const Heading = styled.h2`
 `;
 
 const ForecastGroup = styled.div`
+  --scrollbar-width: 6px;
+
   display: flex;
   flex-direction: column;
   gap: ${16 / 16}rem;
+  overflow-y: scroll;
+
+  /* Moving scrollbar to the parent's border. */
+  margin-inline-end: calc(-1 * var(--inline-padding));
+  padding-inline-end: calc(var(--inline-padding) - var(--scrollbar-width));
+
+  /* Webkit browsers offer greater styling control. */
+  &::-webkit-scrollbar {
+    width: var(--scrollbar-width);
+  }
+
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: var(--clr-neutral-700);
+    border: 1px solid var(--clr-neutral-600);
+    border-radius: 999px;
+  }
+
+  /* Firefox fallback: just means we can't style outline on the thumb part. */
+  @supports (-moz-appearance: none) {
+    scrollbar-width: thin;
+    scrollbar-color: var(--clr-neutral-700) transparent;
+  }
 `;
